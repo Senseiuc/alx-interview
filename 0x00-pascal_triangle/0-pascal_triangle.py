67ee65d8c0bd954819e44698aa7b91ff3f4e2507
senseiuc <dhinhojboy@gmail.com>
2023-06-30 15:26:44 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A function that returns a pascal triangle of n
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return ([])
+    p_list = [[1]]
+    temp = []
+    for i in range(n - 1):
+        c_list = p_list[-1]
+        temp.append(1)
+        for i in range(len(c_list) - 1):
+            temp.append(c_list[i] + c_list[i + 1])
+        temp.append(1)
+        p_list.append(temp)
+        temp = []
+    return (p_list)
