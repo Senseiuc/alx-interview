65f88f9d5493b73b881480f8d174eb41388a8d67
senseiuc <dhinhojboy@gmail.com>
2023-08-17 15:37:08 +0000

    rotate a matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+0. Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotates an m by n 2D matrix in place.
+    """
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
