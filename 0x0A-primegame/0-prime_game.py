8ffcc15581e24726d4dc9c45b02a8eab72751439
SenseiUC <62717218+Senseiuc@users.noreply.github.com>
2023-09-05 09:42:20 +0100

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+0. Prime Game
+"""
+
+
+def prime_count(num):
+    """
+    Returns the number of prime factors
+    """
+    count = 0
+    if num == 1:
+        return count
+    if num == 2:
+        return 1
+    numbs = [i for i in range(2, num + 1)]
+    for i in numbs:
+        is_prime = True
+        for j in range(2, i):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            count += 1
+            numbs = [k for k in numbs if k % i != 0]
+    return count
+
+
+def isWinner(x, nums):
+    """
+    x is the number of rounds and nums is an array of n
+    Return: name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    """
+    ben, jane = 0, 0
+    for i in range(x):
+        p_count = prime_count(nums[i])
+        if p_count % 2 == 0:
+            ben += 1
+        else:
+            jane += 1
+    if ben > jane:
+        return 'Ben'
+    elif jane > ben:
+        return 'Maria'
+    return None
+
+
+# print(isWinner(100, [i for i in range(1, 101)]))
+# print(isWinner(3, [4, 5, 1]))
+# print(isWinner(2, [3, 5]))
